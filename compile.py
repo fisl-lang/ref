6051d23a35c8057badb80a960eba78bdb90de35e
S1mon <jaforsi.kozak@gmail.com>
2025-10-18 14:56:53 +0200

    implemented statements partially
    -label
    -let
    -print
    -if

--- compile.py
+++ compile.py
@@ -3,9 +3,6 @@ import sys
 
 
 
-out = []
-def emit(opcode, attr):
-    out.append((opcode, attr))
 
 
 def lex(line):
@@ -28,6 +25,35 @@ def lex(line):
 
 
 
+out = []
+def emit(opcode, attr=None):
+    out.append((opcode, attr))
+
+labels = {}
+vars = {}
+alloc = 0
+
+def error(msg):
+    print(f'Error: {msg}')
+    sys.exit(0)
+
+
+
+def read(x):
+    if x.isdigit():
+        emit('ldi', int(x))
+    elif x in vars:
+        emit('lda', vars[x])
+    else:
+        error(f'Unable to read from: {x}')
+
+def write(x):
+    global alloc
+    if x not in vars:
+        vars[x] = alloc
+        alloc += 1
+    emit('sta', vars[x])
+
 
 
 def compile(path):
@@ -38,12 +64,54 @@ def compile(path):
         if not line.strip(): continue
 
         parts = lex(line)
-
         print(parts)
 
-    
+        match parts:
+            case ['label', name]: labels[name] = len(out)
+            case ['let', tar, 'be', src]:
+                read(src)
+                write(tar)
+            case ['let', tar, 'be', a, op, b]:
+                read(b)
+                emit('tfr')
+                read(a)
+
+                match op:
+                    case 'plus':  emit('add')
+                    case 'minus': emit('sub')
+                    case x: error(f"Unknown operation: {x}")
+
+                write(tar)
+
+            case ['print', tar]:
+                read(tar)
+                emit('print', 0)
+
+            case ['if', a, comp, b, 'goto', name]:
+                read(b)
+                emit('tfr')
+                read(a)
+
+                match comp:
+                    case 'equal':   emit('ceq')
+                    case 'unequal': emit('cue')
+                    case 'lesser':  emit('clt')
+                    case 'greater': emit('cgt')
+
+                if name in labels:
+                    emit('cgo', labels[name])
+                else:
+                    error("Label '{name}' not defined.\nNote: Labels cannot be forward referenced.")
+
+                
+            
+            case _:
+                error(f"Unable to compile: {line}")
+
 
 
 
 compile(sys.argv[1])
+
+print(out)
 
